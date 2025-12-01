394d5b2d725252de8c0497a505a79ce227dc55c1
Yana Koryakina <y.koryakina@g.nsu.ru>
2025-12-01 17:47:01 +0700

    Исправления замечаний

---
+++ js/constants.js
@@ -0,0 +1,30 @@
+export const COMMENT_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра...',
+  'Моя бабушка случайно чихнула с фотоаппаратом...',
+  'Я поскользнулся на банановой кожуре…',
+  'Лица у людей на фотке перекошены…',
+];
+
+export const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+export const OBJECTS_COUNT = 25;
+
+export const MAX_PHOTO_ID = 25;
+export const MAX_AVATAR_ID = 6;
+
+export const MAX_COMMENTS_COUNT = 30;
+
+export const MIN_LIKES = 15;
+export const MAX_LIKES = 200;
+
--- js/data.js
+++ js/data.js
@@ -1,52 +1,56 @@
 import {getRandomInteger, createRandomIdFromRangeGenerator} from './util.js';
+import { COMMENT_MESSAGES, NAMES, OBJECTS_COUNT } from './constants.js';
+import {
+  MAX_PHOTO_ID,
+  MAX_AVATAR_ID,
+  MAX_COMMENTS_СOUNT,
+  MIN_LIKES,
+  MAX_LIKES
+} from './data/constants.js';
 
-const COMMENT_MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра...',
-  'Моя бабушка случайно чихнула с фотоаппаратом...',
-  'Я поскользнулся на банановой кожуре…',
-  'Лица у людей на фотке перекошены…',
-];
+let lastCommentId = 0;
 
-const NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-];
+function generateCommentId() {
+  lastCommentId++;
+  return lastCommentId;
+}
 
-const OBJECTS_COUNT = 25;
+const generateId = createRandomIdFromRangeGenerator(1, MAX_PHOTO_ID);
 
-const generateAdressId = createRandomIdFromRangeGenerator(1, 25);
-const generateId = createRandomIdFromRangeGenerator(1, 25);
-const generateCommentId = createRandomIdFromRangeGenerator(1, 1000);
 
+function generateMessage() {
+  const sentencesCount = getRandomInteger(1, 2);
+
+  const sentences = Array.from({ length: sentencesCount }, () => {
+    const randomIndex = getRandomInteger(0, COMMENT_MESSAGES.length - 1);
+    return COMMENT_MESSAGES[randomIndex];
+  });
+
+  return sentences.join(' ');
+}
 
 const createComment = () => ({
   id: generateCommentId(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: COMMENT_MESSAGES[getRandomInteger(0, COMMENT_MESSAGES.length - 1)],
+  avatar: `img/avatar-${getRandomInteger(1, MAX_AVATAR_ID)}.svg`,
+  message: generateMessage(),
   name: NAMES[getRandomInteger(0, NAMES.length - 1)],
 });
 
 const createPhotoData = () => {
-  const commentsCount = getRandomInteger(0, 30);
+  const commentsCount = getRandomInteger(0, MAX_COMMENTS_СOUNT);
 
-  const comments = Array.from(
+  const comments = Array.from (
     { length: commentsCount },
     createComment
   );
 
+  const photoId = generateId();
+
   return {
-    id: generateId(),
-    url: `photos/${generateAdressId()}.jpg`,
+    id: photoId,
+    url: `photos/${photoId}.jpg`,
     description: 'Какое-то описание',
-    likes: getRandomInteger(15, 200),
+    likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
     comments,
   };
 };
